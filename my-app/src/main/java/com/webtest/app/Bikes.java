548c840588e38134dbbe60d4edb0c49f543fb2b8
Lavanya <lavan.chandrasekar@gmail.com>
2017-07-04 21:49:45 +1200

    Practice 4a : Inheritance

---
+++ my-app/src/main/java/com/webtest/app/Bikes.java
@@ -0,0 +1,18 @@
+package com.webtest.app;
+
+/**
+ * Created by user on 4/07/2017.
+ */
+public class Bikes extends Cars {
+
+    public static void main(String[] args)
+    {
+        Bikes b = new Bikes();
+
+
+
+
+
+    }
+
+}
