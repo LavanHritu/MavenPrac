76da38b1736c02ab4922f9d1e884f2c954cd4317
Lavanya <lavan.chandrasekar@gmail.com>
2017-07-05 20:56:17 +1200

    Practice 4a : Inheritance

--- my-app/src/main/java/com/webtest/app/Bikes.java
+++ my-app/src/main/java/com/webtest/app/Bikes.java
@@ -7,12 +7,10 @@ public class Bikes extends Cars {
 
     public static void main(String[] args)
     {
+        String type;
         Bikes b = new Bikes();
-
-
-
-
-
+        type = b.getcarstype(1);
+        System.out.println(type);
     }
 
 }
